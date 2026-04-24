04ac85ad4bddf91bee5369548ce77ed270bdf964
Rakshitkumar_67 <rakshitkumar0606@gmail.com>
2026-04-24 11:28:41 +0530

    Revert config back to Cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,4 @@
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  cloudflare: false,
-  tanstackStart: {
-    server: {
-      preset: 'vercel'
-    }
-  }
-});
+export default defineConfig();
